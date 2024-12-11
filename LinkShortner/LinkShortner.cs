cf80bdfd827f67ae8f62af5cb5d0fcc56ff72bdd
Omar Awwad <omar.awad.eldesouky@gmail.com>
2024-12-11 16:20:38 +0200

    Add project files.

---
+++ LinkShortner/LinkShortner.cs
@@ -0,0 +1,41 @@
+﻿namespace LinkShortner
+{
+    internal class LinkShortner
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://www.youtube.com/watch?v=77jIzgvCIYY";
+
+            var shortenedUrl = await ShortenAsync(url);
+
+
+            Console.WriteLine($"Shortened Url: {shortenedUrl}");
+        }
+        public static async Task<string> ShortenAsync(string url)
+        {
+
+            string apiUrl = "https://is.gd/create.php";
+
+            string shortenedUrl = null;
+
+            string requestUrl = $"{apiUrl}?format=simple&url={Uri.EscapeDataString(url)}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+                    response.EnsureSuccessStatusCode();
+
+                    shortenedUrl = await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error: {e.Message}");
+                }
+            }
+            return shortenedUrl;
+        }
+    }
+}
